a2699505216664b6d8f2b0c057c213df7fab6787
Lexa013 <alexisvanstaen@gmail.com>
2025-01-16 23:41:25 +0100

    Initial commit

---
+++ src/MailDump.App/MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace MailDump.App;
+
+partial class MainForm
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        fileListBox = new System.Windows.Forms.CheckedListBox();
+        addFileButton = new System.Windows.Forms.Button();
+        removeFileButton = new System.Windows.Forms.Button();
+        extractButton = new System.Windows.Forms.Button();
+        SuspendLayout();
+        // 
+        // fileListBox
+        // 
+        fileListBox.FormattingEnabled = true;
+        fileListBox.Location = new System.Drawing.Point(12, 12);
+        fileListBox.Name = "fileListBox";
+        fileListBox.Size = new System.Drawing.Size(335, 148);
+        fileListBox.TabIndex = 0;
+        // 
+        // addFileButton
+        // 
+        addFileButton.Location = new System.Drawing.Point(353, 12);
+        addFileButton.Name = "addFileButton";
+        addFileButton.Size = new System.Drawing.Size(147, 31);
+        addFileButton.TabIndex = 1;
+        addFileButton.Text = "Ajouter";
+        addFileButton.UseVisualStyleBackColor = true;
+        addFileButton.Click += addFileButton_Click;
+        // 
+        // removeFileButton
+        // 
+        removeFileButton.Location = new System.Drawing.Point(353, 49);
+        removeFileButton.Name = "removeFileButton";
+        removeFileButton.Size = new System.Drawing.Size(147, 31);
+        removeFileButton.TabIndex = 2;
+        removeFileButton.Text = "Supprimer";
+        removeFileButton.UseVisualStyleBackColor = true;
+        removeFileButton.Click += removeFileButton_Click;
+        // 
+        // extractButton
+        // 
+        extractButton.Location = new System.Drawing.Point(12, 168);
+        extractButton.Margin = new System.Windows.Forms.Padding(3, 5, 3, 3);
+        extractButton.Name = "extractButton";
+        extractButton.Size = new System.Drawing.Size(488, 35);
+        extractButton.TabIndex = 4;
+        extractButton.Text = "Extraire";
+        extractButton.UseVisualStyleBackColor = true;
+        extractButton.Click += extractButton_Click;
+        // 
+        // MainForm
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        BackColor = System.Drawing.SystemColors.Control;
+        ClientSize = new System.Drawing.Size(507, 214);
+        Controls.Add(extractButton);
+        Controls.Add(removeFileButton);
+        Controls.Add(addFileButton);
+        Controls.Add(fileListBox);
+        Location = new System.Drawing.Point(15, 15);
+        Text = "MailDump";
+        ResumeLayout(false);
+    }
+
+    private System.Windows.Forms.Button extractButton;
+
+    private System.Windows.Forms.Button addFileButton;
+    private System.Windows.Forms.Button removeFileButton;
+
+    private System.Windows.Forms.CheckedListBox fileListBox;
+
+    #endregion
+}
---
+++ src/MailDump.App/MainForm.cs
@@ -0,0 +1,198 @@
+using System.Diagnostics;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using Message = MsgReader.Mime.Message;
+
+namespace MailDump.App;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+        
+        this.FormBorderStyle = FormBorderStyle.FixedSingle;
+    }
+
+    private string _saveFileFolderPath = "";
+    private string _tempDirPath = Path.Combine(Directory.GetCurrentDirectory(), "temp");
+    
+
+    private void addFileButton_Click(object sender, EventArgs e)
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.Multiselect = true;
+        openFileDialog.Filter = "Mail files (*.eml)|*.eml";
+        openFileDialog.ShowDialog();
+        
+        foreach (var fileName in openFileDialog.FileNames)
+        {
+            fileListBox.Items.Add(fileName);
+        }
+    }
+    private void removeFileButton_Click(object sender, EventArgs e)
+    {
+        for (var i = 0; i < fileListBox.Items.Count; i++)
+        {
+            if (fileListBox.GetItemChecked(i))
+            {
+                fileListBox.Items.RemoveAt(i);
+                // When removing an item all index are decreased by 1 so we need to counter that 
+                i--;
+            }
+        }
+    }
+    
+    private async void extractButton_Click(object sender, EventArgs e)
+    {
+        using (var folderBrowser = new FolderBrowserDialog())
+        {
+            folderBrowser.ShowNewFolderButton = false;
+            folderBrowser.Description = "Sélectionner le dossier d'extraction";
+
+            if (folderBrowser.ShowDialog() != DialogResult.OK)
+                return;
+            
+            _saveFileFolderPath = folderBrowser.SelectedPath;
+
+            Console.WriteLine($"Files will be saved to: {_saveFileFolderPath}");
+
+            var tasks = fileListBox.Items.Cast<string>()
+                .Select(async file => await ProcessFile(file)).ToList();
+
+            var outputs = await Task.WhenAll(tasks);
+            
+            foreach (var output in outputs.Where(output => output is not null))
+            {
+                try
+                {
+                    File.Move(output!, Path.Combine(_saveFileFolderPath, Path.GetFileName(output!)), true);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"Failed to move file: {output}");
+                    continue;
+                }
+            }
+            
+            MessageBox.Show("Extraction terminée", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        } 
+    }
+    
+    private async Task<string?> ProcessFile(string filePath)
+    {
+        FileInfo fileInfo;
+        
+        try
+        {
+            fileInfo = new FileInfo(filePath);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Failed to create file info for: {(string)filePath}");
+            return null;
+        }
+        
+        Directory.CreateDirectory(_tempDirPath);
+        
+        var fileName = Path.GetFileNameWithoutExtension(filePath);
+
+        Process convertProcess = new Process();
+        convertProcess.StartInfo.FileName = "java";
+        convertProcess.StartInfo.Arguments = $"-jar {Directory.GetCurrentDirectory()}\\toolset\\emailconverter.jar -o \"{_tempDirPath}\\{fileName}.pdf\" \"{filePath}\"";
+        convertProcess.StartInfo.WorkingDirectory = $"{Directory.GetCurrentDirectory()}\\toolset";
+        convertProcess.StartInfo.CreateNoWindow = true;
+        convertProcess.StartInfo.RedirectStandardOutput = true;
+        convertProcess.StartInfo.UseShellExecute = false;
+
+        string originalPath = $"{_tempDirPath}\\{fileName}.pdf";
+        
+        try
+        {
+            await Task.Run(() =>
+            {
+                convertProcess.Start();
+                convertProcess.WaitForExit();
+            });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Failed to convert eml file to pdf: {fileName}");
+            return null;
+        }
+
+        Console.WriteLine($"Successfully converted eml file to pdf: {fileName}");
+        
+        // Read attachements
+
+        Message message;
+        try
+        {
+            message = Message.Load(fileInfo);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Failed to load .eml file to get attachements: {fileName}");
+            return originalPath;
+        }
+        
+        if (message.Attachments is null)
+        {
+            Console.WriteLine($"No attachments were provided to convert eml file to pdf: {fileName}");
+            return originalPath;
+        }
+
+        var pdfAttachements = message.Attachments.Where(a => Path.GetExtension(a.FileName) == ".pdf").ToList();
+
+        if (!pdfAttachements.Any())
+        {
+            Console.WriteLine($"Message contains attachements but none of them are a pdf file: {fileName}");
+            return originalPath;
+        }
+        
+        // Attach avery attachement to orginal pdf
+        for (var i = 0; i < pdfAttachements.Count; i++)
+        {
+            var pdfFilePath = $"{_tempDirPath}\\{fileName}_{i}.pdf";
+
+            await File.WriteAllBytesAsync(pdfFilePath, pdfAttachements[i].Body);
+
+            var original = PdfReader.Open(originalPath, PdfDocumentOpenMode.Modify);
+            var newPdf = PdfReader.Open(pdfFilePath, PdfDocumentOpenMode.Import);
+            
+            MergePages(newPdf, original);
+            
+            try
+            {
+                original.Save(originalPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to save file to pdf: {fileName}");
+            }
+            
+            original.Dispose();
+            newPdf.Dispose();
+
+            try
+            {
+                File.Delete(pdfFilePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to delete temp file: {pdfFilePath}");
+                return originalPath;
+            }
+        }
+        
+        return originalPath;
+    }
+
+    private void MergePages(PdfDocument from, PdfDocument to)
+    {
+        for (var i = 0; i < from.PageCount; i++)
+        {
+            to.AddPage(from.Pages[i]);
+        }
+    }
+}
